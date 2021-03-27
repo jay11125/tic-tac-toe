1a91316998570e09e2c5ce892a1ed23899f5fdcf
jay11125 <70865848+jay11125@users.noreply.github.com>
2021-03-28 00:10:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ var h = document.getElementById("winner");
 var box = document.getElementsByClassName("box");
 var x = Array.from(box);
 var g = 0;
+
+h.innerHTML="It is Player O's turn"
+
 function check(player,a,b,c){
     if (x[a].innerText === player && x[b].innerText === player && x[c].innerText === player) {
         h.innerHTML = `Player ${player} wins!!!`;
@@ -28,6 +31,12 @@ x.forEach(element => {
                 element.innerText = current_player;
                 current_player = "O"
             }
+        }
+        if(current_player==="O"){
+            h.innerHTML=`It is Player O's turn`
+        }
+        else if(current_player==="X"){
+            h.innerHTML=`It is Player X's turn`
         }
         if (element.innerHTML !== null || element.innerHTML !== "") {
 
